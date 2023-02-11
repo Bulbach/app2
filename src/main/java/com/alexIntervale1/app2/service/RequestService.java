77a66fcf25f3a5b5f8558f3c29c8384a0d15fc8d
Bulbash <zverovik@yahoo.com>
2023-02-11 15:42:15 +0300

    refactored class RequestService

--- src/main/java/com/alexIntervale1/app2/service/RequestService.java
+++ src/main/java/com/alexIntervale1/app2/service/RequestService.java
@@ -2,19 +2,19 @@ package com.alexIntervale1.app2.service;
 
 import com.alexIntervale1.app2.dto.RequestDto;
 import com.alexIntervale1.app2.exception.CustomAppException;
+import com.alexIntervale1.app2.jdbc.named.RequestNamedParameterJdbcImpl;
 import com.alexIntervale1.app2.mapper.RequestMapper;
 import com.alexIntervale1.app2.model.RequestMessage;
-import com.alexIntervale1.app2.jdbc.named.RequestNamedParameterJdbcImpl;
 import com.alexIntervale1.app2.worker.Worker;
 import com.google.gson.Gson;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.activemq.Message;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
 import javax.jms.JMSException;
+import javax.jms.Message;
 import javax.jms.TextMessage;
 
 @Service
@@ -24,7 +24,7 @@ import javax.jms.TextMessage;
 public class RequestService {
 
 //    private final RequestRepo repo;
-//    private final RequestJdbcRepoImpl repo;
+    //    private final RequestJdbcRepoImpl repo;
     private final RequestNamedParameterJdbcImpl repo;
     private final Gson gson;
 
@@ -36,6 +36,20 @@ public class RequestService {
     public void receiveMessage(final Message message) throws CustomAppException {
 
         log.info("Received message " + message);
+        String numberRequest = getNumberRequest(message);
+
+        String json = getTextFromMessage((TextMessage) message);
+
+        RequestDto requestDto = getRequestDto(json);
+
+        RequestMessage requestMessage = getRequestMessage(numberRequest, requestDto);
+
+        repo.save(requestMessage);
+        log.info("Полученны объект сохранненый в базу " + requestMessage);
+
+    }
+
+    private String getNumberRequest(Message message) throws CustomAppException {
         String numberRequest = null;
         try {
             numberRequest = message.getJMSCorrelationID();
@@ -44,28 +58,34 @@ public class RequestService {
             throw new CustomAppException(e);
         }
         log.info(numberRequest);
+        return numberRequest;
+    }
 
+    private RequestMessage getRequestMessage(String numberRequest, RequestDto requestDto) {
+        RequestMessage requestMessage = new RequestMessage();
+        requestMessage.setCorrelationID(numberRequest);
+        log.info("Созданіе нового об'екта" + requestMessage);
+
+        requestMapper.updateRequestMessageFromDto(requestDto, requestMessage);
+        log.info("Об'ект после конвертацыі " + requestMessage);
+        return requestMessage;
+    }
+
+    private RequestDto getRequestDto(String json) {
+        RequestDto requestDto = gson.fromJson(json, RequestDto.class);
+        log.info(String.valueOf(requestDto));
+        return requestDto;
+    }
+
+    private String getTextFromMessage(TextMessage message) throws CustomAppException {
         String json = "";
-        TextMessage textMessage = (TextMessage) message;
+        TextMessage textMessage = message;
         try {
             json = textMessage.getText();
         } catch (JMSException e) {
             log.warn("Проблемы при получении текста ответа, метод receiveMessage ", e);
             throw new CustomAppException(e);
         }
-
-        RequestDto requestDto = gson.fromJson(json, RequestDto.class);
-        log.info(String.valueOf(requestDto));
-
-        RequestMessage message1 = new RequestMessage();
-        message1.setCorrelationID(numberRequest);
-        log.info("Созданіе нового об'екта" + message1);
-
-        requestMapper.updateRequestMessageFromDto(requestDto, message1);
-        log.info("Об'ект после конвертацыі " + message1);
-
-        repo.save(message1);
-        log.info(String.valueOf(message1));
-
+        return json;
     }
 }
